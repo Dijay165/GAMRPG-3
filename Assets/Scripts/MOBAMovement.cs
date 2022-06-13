f7b39309cb7f7eac1778919513308c68c45bcc2e
dijay165 <arjay.diocton@benilde.edu.ph>
2022-06-13 11:17:42 +0800

    Minion Asset

--- Assets/Scripts/MOBAMovement.cs
+++ Assets/Scripts/MOBAMovement.cs
@@ -7,16 +7,27 @@ using UnityEngine.AI;
 public class MOBAMovement : MonoBehaviour
 {
     NavMeshAgent agent;
+    Animator anim;
+    Vector2 smoothDeltaPosition = Vector2.zero;
+    Vector2 velocity = Vector2.zero;
 
+    [SerializeField]
+    float rotateSpeed;
+
+    float rotateVelocity;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        
         if (Input.GetMouseButtonDown(1))
         {
             RaycastHit hit;
@@ -24,7 +35,21 @@ public class MOBAMovement : MonoBehaviour
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
                 agent.SetDestination(hit.point);
+
+                //Quaternion rotationLookAt = Quaternion.LookRotation(hit.point - transform.position);
+                //float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationLookAt.eulerAngles.y,
+                //    ref rotateVelocity, rotateSpeed * (Time.deltaTime * 5));
+
+                //transform.eulerAngles = new Vector3(0, rotationY, 0);
+
+                transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, hit.point, rotateSpeed * Time.deltaTime, 0.0f));
             }
+          //  anim.SetTrigger("Attack1");
+            //anim.SetTrigger("Attack2");
+          //  anim.SetBool("dead", true);
         }
+
+        anim.SetFloat("Velocity X", agent.velocity.x, 0.01f, Time.deltaTime);
+        anim.SetFloat("Velocity Z", agent.velocity.z, 0.01f, Time.deltaTime);
     }
 }
