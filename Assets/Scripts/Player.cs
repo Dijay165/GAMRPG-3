949484d5248caf8f6d3237b543b607d5bc7574a8
dijay165 <arjay.diocton@benilde.edu.ph>
2022-07-02 13:27:37 +0800

    Set default values

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : Unit
 {
+    NavMeshAgent nav;
 
+    private void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        nav.speed = unitStat.startingMovementSpeed;
+    }
 }
